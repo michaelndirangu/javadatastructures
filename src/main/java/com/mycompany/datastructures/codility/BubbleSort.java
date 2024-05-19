32d2ddb0c9baa9575757d50f3e7a2b0ddbdd3566
michaelndirangu <ndirangumichael33@gmail.com>
2024-05-19 22:03:16 +0300

    minor

---
+++ src/main/java/com/mycompany/datastructures/codility/BubbleSort.java
@@ -0,0 +1,10 @@
+package com.mycompany.datastructures.codility;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static  void main(String[] args) {
+        int nums[] = {6,5,2,9,4};
+        System.out.println(Arrays.toString(nums));
+    }
+}
